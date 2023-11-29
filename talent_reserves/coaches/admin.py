5f4a44826c7a34c79e8786e089d2a1612b6056c1
Зайковский Всеволод <4lk4st@gmail.com>
2023-11-29 11:02:55 +0700

    edit admin panel for coaches

--- talent_reserves/coaches/admin.py
+++ talent_reserves/coaches/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
 from .models import Coach, Direction
 
 
-admin.site.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ["photography", "surname", "name", "patronymic",
+                    "direction", "achievements", "birthday"]
+    ordering = ["surname"]
+
+    def photography(self, obj):
+        try:
+            url = obj.photo.url
+            return mark_safe(f'<img src={url} width="100" height="100">')
+        except:
+            return "No photo available"
+       
+    def	direction(self, obj):
+        return ', '.join(
+            [direction.title for direction in obj.directions.all()]
+        )
+
+
+admin.site.register(Coach, CoachAdmin)
 admin.site.register(Direction)
