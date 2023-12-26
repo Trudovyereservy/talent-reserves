43c5133e6c85a5c8b7537bfdcc7e31d6ab752103
Viktoria Latysheva <vikkilat1989@yandex.ru>
2023-12-26 20:42:11 +0300

    common pagination

--- talent_reserves/api/views.py
+++ talent_reserves/api/views.py
@@ -9,7 +9,7 @@ from coaches.models import Coach
 from news.models import News
 
 from .filters import PostFilter, CoachFilter, NewsFilter
-from .pagination import BlogPagination, CoachPagination, NewsPagination
+from .pagination import CommonPagination
 from .serializers import CoachSerializer, NewsSerializer, PostSerializer
 
 
@@ -21,7 +21,7 @@ class CoachViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Coach.objects.all()
     serializer_class = CoachSerializer
-    pagination_class = CoachPagination
+    pagination_class = CommonPagination
     filter_backends = [DjangoFilterBackend, ]
     filterset_class = CoachFilter
 
@@ -30,7 +30,7 @@ class NewsViewSet(viewsets.ReadOnlyModelViewSet):
     """Вьюсет для отображения списка новостей."""
     queryset = News.objects.all()
     serializer_class = NewsSerializer
-    pagination_class = NewsPagination
+    pagination_class = CommonPagination
     filter_backends = [DjangoFilterBackend, ]
     filterset_class = NewsFilter
 
@@ -44,7 +44,7 @@ class PostListViewSet(viewsets.ReadOnlyModelViewSet):
     """Вьюсет для работы с постами."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    pagination_class = BlogPagination
+    pagination_class = CommonPagination
     filter_backends = [DjangoFilterBackend, ]
     filterset_class = PostFilter
 
