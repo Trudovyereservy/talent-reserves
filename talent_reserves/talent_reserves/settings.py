180cf3c5c58d587a94af73696d436a764f1a85aa
Антон Лучик <Intemic@yandex.ru>
2024-04-22 21:43:07 +0500

    Deleting sensitive data

--- talent_reserves/talent_reserves/settings.py
+++ talent_reserves/talent_reserves/settings.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default=' ')
+SECRET_KEY = os.getenv('SECRET_KEY',  default=get_random_secret_key())
 
 DEBUG = True
 
