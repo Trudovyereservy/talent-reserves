76c0431bffabf35cb5c05750731f8ecc9270a697
Anton <A60874022@yandex.ru>
2024-06-13 19:58:21 +0500

    fix: celery

--- talent_reserves/talent_reserves/tasks.py
+++ talent_reserves/talent_reserves/tasks.py
@@ -2,16 +2,13 @@ from django.core.mail import send_mail
 from celery import shared_task
 from django.conf import settings
 
+
 @shared_task()
 def send_feedback_email_task():
     """Sends an email when the feedback form has been submitted."""
     send_mail(
                 'New Feedback Received',
-                f'Name: Вы зВы зарегестрированы ',
-           
                 settings.EMAIL_HOST_USER,
                 [settings.EMAIL_RECIPIENT],
                 fail_silently=False
             )
-  
-
