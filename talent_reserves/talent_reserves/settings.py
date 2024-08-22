a871e579b9c54e238297c1f1dad46c4788533f02
Леонид Агалаков <agalakov.la@yandex.ru>
2024-08-22 15:22:31 +0500

    First deploy on new server

--- talent_reserves/talent_reserves/settings.py
+++ talent_reserves/talent_reserves/settings.py
@@ -102,7 +102,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
