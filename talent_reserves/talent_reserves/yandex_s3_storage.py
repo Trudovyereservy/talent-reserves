1f940b5f01d1c7b6a5df7e3756ea45c76999a342
Леонид Агалаков <agalakov.la@yandex.ru>
2024-08-23 17:39:49 +0500

    First deploy on new server

--- talent_reserves/talent_reserves/yandex_s3_storage.py
+++ talent_reserves/talent_reserves/yandex_s3_storage.py
@@ -1,16 +1,16 @@
-from storages.backends.s3boto3 import S3Boto3Storage
+# from storages.backends.s3boto3 import S3Boto3Storage
 
-from .settings import YANDEX_BUCKET_NAME, YANDEX_PUBLIC_MEDIA_LOCATION
-
-
-class ClientDocsStorage(S3Boto3Storage):
-    bucket_name = YANDEX_BUCKET_NAME
-    custom_domain = "storage.yandexcloud.net"
-    secure_urls = True
+# from .settings import YANDEX_BUCKET_NAME, YANDEX_PUBLIC_MEDIA_LOCATION
 
 
-class ClientMediaStorage(S3Boto3Storage):
-    bucket_name = YANDEX_BUCKET_NAME
-    location = YANDEX_PUBLIC_MEDIA_LOCATION
-    file_overwrite = False
-    secure_urls = True
+# class ClientDocsStorage(S3Boto3Storage):
+#     bucket_name = YANDEX_BUCKET_NAME
+#     custom_domain = "storage.yandexcloud.net"
+#     secure_urls = True
+
+
+# class ClientMediaStorage(S3Boto3Storage):
+#     bucket_name = YANDEX_BUCKET_NAME
+#     location = YANDEX_PUBLIC_MEDIA_LOCATION
+#     file_overwrite = False
+#     secure_urls = True
