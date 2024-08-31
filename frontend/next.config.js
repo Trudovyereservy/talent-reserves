968c4f35c7c52181118a2316f2d748e01f947aba
Леонид Агалаков <agalakov.la@yandex.ru>
2024-08-31 15:22:39 +0500

    Update frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
     output: 'standalone',
     images: {
         domains: ['talent-reserves.storage.yandexcloud.net', 'localhost:3000', '127.0.0.1:3000'],
