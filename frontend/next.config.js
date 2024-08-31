18b2ffa61f5c025fecc21ced745fb407ab879179
Леонид Агалаков <agalakov.la@yandex.ru>
2024-08-31 15:59:30 +0500

    Disable CORS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     },
     output: 'standalone',
     images: {
-        domains: ['talent-reserves.storage.yandexcloud.net', 'localhost:3000', '127.0.0.1:3000'],
-      },
+      domains: ['talent-reserves.storage.yandexcloud.net', 'localhost:3000', '127.0.0.1:3000'],
+    },
 }
 
 module.exports = nextConfig
