ca126c3082cc61705efca7bf62714ad822dde096
Леонид Агалаков <agalakov.la@yandex.ru>
2024-09-01 23:24:17 +0500

    Update frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,27 @@
-import type { Metadata } from 'next';
-import { CoachList } from '@/components/CoachCards/CoachList/CoachList';
-import { DescriptionPages } from '@/components/DescriptionPages/DescriptionPages';
-import { Pagination } from '@/components/Pagination/Pagination';
-import { descriptionPages } from '@/utils/constants';
-export const metadata: Metadata = {
-  title: { absolute: 'Трудовые резервы | Тренерский состав' },
-  description:
-    'Эта страница создана для демонстрации блоков и элементов, которые используются на сайте...',
-};
+import { CardsList } from '@/components/CardsList/CardsList';
+import { CardsNews } from '@/components/CardsNews/CardsNews';
+import { CompanyInfo } from '@/components/CompanyInfo/CompanyInfo';
+import { Faq } from '@/components/Faq/Faq';
+import Form from '@/components/Form/Form';
+import { MainBanner } from '@/components/main-banner/main-banner';
+import SwiperProvider from '@/components/ProviderComponents/SwiperProvider';
+
+import { cardsNewsMain } from '../utils/constants';
+
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <>
-      <DescriptionPages descriptionPages={descriptionPages} />
-      <CoachList />
-      {/* TODO: Update with functionality */}
-      <Pagination totalCards={117} currentPage={3} />
+      <div className={styles.mainbanner__wrapper}>
+        <MainBanner />
+        <CompanyInfo />
+        <CardsList />
+        <SwiperProvider />
+        <Form />
+        <Faq />
+        <CardsNews cardsNews={cardsNewsMain} />
+      </div>
     </>
   );
 }
