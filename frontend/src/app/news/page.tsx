900fc221ce37731b7d1b26b051c9f0067a392f02
Леонид Агалаков <agalakov.la@yandex.ru>
2024-09-02 00:37:18 +0500

    Update frontend

--- frontend/src/app/news/page.tsx
+++ frontend/src/app/news/page.tsx
@@ -1,9 +1,27 @@
 import type { Metadata } from 'next';
-
 import { DescriptionPages } from '@/components/DescriptionPages/DescriptionPages';
 import { NewsCardsList } from '@/components/NewsCards/NewsCardsList/NewsCardsList';
 import { Pagination } from '@/components/Pagination/Pagination';
 import { newsCards, descriptionPages } from '@/utils/constants';
+
+// Убедитесь, что этот тип соответствует вашему определению
+interface NewsCard {
+  id: string;
+  title: string;
+  tags?: string[];
+  // другие свойства
+}
+
+interface NewsCardProps {
+  id: string;
+  title: string;
+  tags: React.ReactNode;
+}
+
+const convertedNewsCards: NewsCardProps[] = newsCards.map(card => ({
+  ...card,
+  tags: card.tags ? card.tags.map(tag => <span key={tag}>{tag}</span>) : null,
+}));
 
 export const metadata: Metadata = {
   title: { absolute: 'Трудовые резервы | Новости' },
@@ -19,7 +37,7 @@ export default function NewsPage() {
         <meta name="title" content="Новости" />
       </head>
       <DescriptionPages descriptionPages={descriptionPages} />
-      <NewsCardsList newsCards={newsCards} />
+      <NewsCardsList newsCards={convertedNewsCards} />
       {/* TODO: Update with functionality */}
       <Pagination totalCards={117} currentPage={3} />
     </>
