4a72c841228a178f93a97e120ff84215463909b6
Леонид Агалаков <agalakov.la@yandex.ru>
2024-09-02 00:12:45 +0500

    Update frontend

--- frontend/src/components/NewsCards/NewsCardsList/NewsCardsList.tsx
+++ frontend/src/components/NewsCards/NewsCardsList/NewsCardsList.tsx
@@ -6,18 +6,15 @@ import { useCardCount } from '@/hooks/useCardCount';
 import useWindowSize from '@/hooks/useWindowSize';
 
 import { NewsCard } from '../NewsCard/NewsCard';
-import { NewsCardProp, ITags } from '../NewsCard/NewsCard.props';
+import { NewsCardProps } from '../NewsCard/NewsCard.props';
 
 import styles from './NewsCardsList.module.scss';
 
-const NewsCardsList = ({ newsCards }: { newsCards: NewsCardProp[] }) => {
+const NewsCardsList = ({ newsCards }: { newsCards: NewsCardProps[] }) => {
   const width: number = useWindowSize();
   const count = useCardCount(width, 'newsCardsComponent');
 
-  const visibleNewsCards: NewsCardProp[] = useMemo(
-    () => newsCards.slice(0, count),
-    [newsCards, count],
-  );
+  const visibleNewsCards = useMemo(() => newsCards.slice(0, count), [newsCards, count]);
 
   return (
     <section className={styles.cardslist}>
@@ -30,16 +27,7 @@ const NewsCardsList = ({ newsCards }: { newsCards: NewsCardProp[] }) => {
             description={card.description}
             images={card.images}
             date_published={card.date_published}
-            // tags={card.tags}
-            tags={
-              Array.isArray(card.tags) ? (
-                <>
-                  {card.tags.map((tag: ITags) => (
-                    <span key={tag.id}>{tag.name}</span>
-                  ))}
-                </>
-              ) : null
-            }
+            tags={card.tags}
           />
         ))}
       </ul>
